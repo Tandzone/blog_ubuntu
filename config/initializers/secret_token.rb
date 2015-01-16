3c7e5ebefd6d9007fd2c57f5745f123547597e35
Tanjona ubuntu <tanjonatsiry@gmail.com>
2015-01-16 15:20:38 +0300

    my first commit on ubuntu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '76677613ffd9fe28f003f2d8d56094c6053f1e56d1beb94c8e7015b36a76d53616df2bf8c8461e68c514a341342ed4bc4b35352efe2dc651b8d0826ec5d88f4e'
