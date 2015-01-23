b08f1b8d36930c2450f770141f7c518fda3b6bf3
Tanjona ubuntu <tanjonatsiry@gmail.com>
2015-01-23 14:27:55 +0300

    starting with password

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+
+  validates_presence_of :name, :email, :body
+  validate :article_should_be_published
+
+  default_scope lambda {order('comments.created_at DESC')}
+
+  def article_should_be_published
+    errors.add(:article_id, "is not published yet") if article && !article.published?
+  end
+
+  after_create :email_article_author
+
+  def email_article_author
+    puts "We will notify #{article.user.email} in chapter 9"
+  end
 end
