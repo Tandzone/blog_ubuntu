e4b3f91217f3689e387be99c04c8fe91a8e09e55
tanjona home <tanjonatsiry@gmail.com>
2015-03-07 09:49:37 +0300

    new link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def submit_or_cancel(form, name="Cancel")
-    form.submit + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+  def submit_or_cancel(form, submit_label="Submit", name="Cancel")
+    (form.submit submit_label) + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Blog::Application.routes.draw do
 
   get '/login' => 'sessions#new', :as => "login"
   get '/logout' => 'sessions#destroy', :as => "logout"
+  get '/new-account' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
